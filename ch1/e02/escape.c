11f02db94c858f79f19349eac45f9e83e460457c
Mariusz Gorski <marius.gorski@gmail.com>
2013-11-16 01:57:57 +0100

    Add first two solutions

---
+++ ch1/e02/escape.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Expected output:
+ * h
+ * )
+ * w
+ */
+
+int main() {
+	printf("\h\n");
+	printf("\)\n");
+	printf("\w\n");
+
+	return EXIT_SUCCESS;
+}
