9cd0f4ccde8e612533c8cae8106fe392a209ba22
Mariusz Gorski <marius.gorski@gmail.com>
2013-11-16 18:28:16 +0100

    Add solution to ch1e05

---
+++ ch1/e05/temp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int lower = 0, upper = 300, step = 20;
+
+	printf("  F      C\n");
+	printf("----------\n");	
+
+	float temp;
+	for (temp = upper; temp >= lower; temp -= step) {
+		printf("%3.0f %6.1f\n", temp, (5.0 / 9.0) * (temp - 32));
+	}
+
+	return EXIT_SUCCESS;
+}
